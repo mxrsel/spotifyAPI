d1b3a3a793b0793fd00e9c2712ef725663d37782
mxrsel <marsel14082006@gmail.com>
2025-01-08 18:34:17 +0600

    created new methods for user for checking authorization by user's token

--- SpotifyApi/models/User.ts
+++ SpotifyApi/models/User.ts
@@ -1,16 +1,25 @@
-import mongoose from "mongoose";
+import mongoose, {Model} from "mongoose";
 import bcrypt from 'bcrypt';
+import {randomUUID} from "node:crypto";
 
 interface User {
     username: string
     password: string
+    token: string
 }
+
+interface UserMethods {
+    passwordCheckout(password: string): Promise<boolean>;
+    generateToken(): void
+}
+
+type UserModel = Model<User, {}, UserMethods>;
 
 const SALT_WORK_FACTOR = 8;
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema<User>({
+const UserSchema = new Schema<User, UserModel, UserMethods>({
     username: {
         type: String,
         required: true,
@@ -19,6 +28,10 @@ const UserSchema = new Schema<User>({
     password: {
         type: String,
         required: true,
+    },
+    token: {
+        type: String,
+        unique: true
     }
 });
 
@@ -33,5 +46,13 @@ UserSchema.pre('save', async function(next) {
     next();
 })
 
-const User = mongoose.model("User", UserSchema);
+UserSchema.methods.passwordCheckout = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function() {
+    this.token = randomUUID();
+};
+
+const User = mongoose.model<User, UserModel>("User", UserSchema);
 export default User;
--- SpotifyApi/routers/userRouter.ts
+++ SpotifyApi/routers/userRouter.ts
@@ -11,6 +11,8 @@ userRouter.post('/', async (req, res, next) => {
             password: req.body.password
         });
 
+        newUser.generateToken();
+
         await newUser.save();
         res.send(newUser);
         return;
@@ -21,4 +23,46 @@ userRouter.post('/', async (req, res, next) => {
         }
         return next(e);
     }
-})
+});
+
+userRouter.post('/session', async (req, res) => {
+    const existsUser = await User.findOne({username: req.body.username});
+
+    if(!existsUser) {
+        res.status(400).send({error: 'User is not found'})
+        return
+    }
+
+    const matchPasswords = await existsUser.passwordCheckout(req.body.password);
+
+    if(!matchPasswords) {
+        res.status(400).send({error: 'Password is wrong'});
+        return
+    }
+
+    existsUser.generateToken();
+    await existsUser.save();
+
+    res.send({message: 'User and password correct', existsUser});
+    return
+});
+
+userRouter.post('/secret', async(req, res) => {
+    const token = req.get('Authorization');
+
+    if(!token) {
+        res.status(400).send({error: 'User token is not found'});
+        return
+    }
+    const user = await User.findOne({token});
+
+    if(!user) {
+        res.status(400).send({error: 'Wrong token'});
+        return
+    }
+    res.send({
+        username: user.username,
+        message: "Nothing there :)"
+    });
+    return
+});
